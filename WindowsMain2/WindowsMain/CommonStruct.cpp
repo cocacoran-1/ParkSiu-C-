0f8873ce889851f2c15ce55c6798e312a97b221a
seewu <59171978+cocacoran-1@users.noreply.github.com>
2023-09-26 16:33:37 +0900

    update

--- WindowsMain2/WindowsMain/CommonStruct.cpp
+++ WindowsMain2/WindowsMain/CommonStruct.cpp
@@ -1,2 +1,9 @@
 #include "stdafx.h"
 #include "CommonStruct.h"
+#include "CommonFunction.h"
+
+RECT CenterRect :: ToRect()
+{
+	RECT rc = Rect::MakeRect(x - width / 2, y - height / 2, width, height);
+	return rc;
+}
--- WindowsMain2/WindowsMain/CommonStruct.h
+++ WindowsMain2/WindowsMain/CommonStruct.h
@@ -2,4 +2,11 @@
 struct CenterRect
 {
 	int x, y, width, height;
+
+	RECT ToRect()
+	{
+		RECT rc;
+		Rect::MakeRect(x - width / 2, y - height / 2, width, height);
+		return rc;
+	}
 };
